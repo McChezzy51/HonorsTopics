57a6d7d824a7a1b71d2ffc07fa382a0e16df3168
Chase Mayer <cmayer2@hwemail.com>
2025-08-29 09:14:39 -0700

    Revert "Incomplete file reader, broken file writer"
    This reverts commit ccb510e178997de5a47f47d99c6527736bc2deb2.

--- FileWriter/MyFileReader.java
+++
@@ -1,4 +0,0 @@
-public class MyFileReader {
-    public static void main(String[] args) {
-    }
-}
--- FileWriter/MyFileWriter.java
+++ FileWriter/MyFileWriter.java
@@ -19,7 +19,7 @@ public class MyFileWriter {
         }
 
         // 2. Using BufferedWriter
-        try (BufferedWrwfefeeeiter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
             bufferedWriter.write(data);
         } catch (IOException e) {
             e.printStackTrace();
